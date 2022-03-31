87f34f67948138137520230406aeff279c1264df
Muniba Ahmed <mahmed93@berkeley.edu>
2022-03-31 13:28:55 -0700

    tests passing

--- user-onboarding/cypress/integration/munibatest.js
+++ user-onboarding/cypress/integration/munibatest.js
@@ -12,4 +12,29 @@ describe("User Onboarding Form!", () => {
     expect(7 - 2).to.equal(5);
     expect(2 + 2).to.not.equal(8);
   });
+
+  it("The proper elements are showing", () => {
+    usernameInput().should("exist");
+    passwordInput().should("exist");
+    emailInput().should("exist");
+
+    cy.contains("Create a User").should("exist");
+  });
+
+  describe("Filling out the inputs", () => {
+    it("can navigate to the site", () => {
+      cy.url().should("include", "localhost");
+    });
+
+    it("can type in the inputs", () => {
+      usernameInput()
+        .should("have.value", "")
+        .type("Muniba Ahmed")
+        .should("have.value", "Muniba Ahmed");
+      passwordInput()
+        .should("have.value", "")
+        .type("marloblackcat")
+        .should("have.value", "marloblackcat");
+    });
+  });
 });
